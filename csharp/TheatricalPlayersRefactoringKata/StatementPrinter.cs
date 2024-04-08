ac720ead6542c81fd8280132d82f483d3f607433
Samuel Gocník <samo1182@gmail.com>
2024-04-08 13:11:20 +0200

    fix: refactor

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -10,8 +10,7 @@ namespace TheatricalPlayersRefactoringKata
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var values = new InvoiceValues(invoice, plays);
-            var totalAmount = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
+            var result = string.Format("Statement for {0}\n", values.CustomerName);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
             foreach(var perf in invoice.Performances) 
@@ -19,9 +18,15 @@ namespace TheatricalPlayersRefactoringKata
                 var play = plays[perf.PlayID];
                 var thisAmount = values.CalculateAmount(play, perf);
                 values.CalculateVolumeCreadits(play, perf);
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                values.CurrentPlay.Add(new CurrentPlay { Name = play.Name, Amount = Convert.ToDecimal(thisAmount / 100), Seats = perf.Audience });;
                 values.Total += thisAmount;
             }
+
+            foreach (var currPlay in values.CurrentPlay)
+            {
+                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", currPlay.Name, Convert.ToDecimal(currPlay.Amount / 100), currPlay.Seats);
+            }
+            
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(values.Total / 100));
             result += String.Format("You earned {0} credits\n", values.Credits);
             return result;
@@ -40,7 +45,7 @@ public class InvoiceValues
 {
     public decimal Total { get; set; }
     public int Credits { get; set; }
-    public string Header { get; set; }
+    public string CustomerName { get; set; }
     public List<CurrentPlay> CurrentPlay { get; set; }
     
     private readonly Invoice _invoice;
@@ -51,6 +56,7 @@ public class InvoiceValues
         _invoice = invoice;
         _plays = plays;
         CurrentPlay = new List<CurrentPlay>();
+        CustomerName = invoice.Customer;
     }
 
     public int CalculateAmount(Play play, Performance perf)
